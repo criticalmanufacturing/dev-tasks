f74f825f30c9febfdb7af9125bb4edd378230145
Pedro Simeão <pesimeao@gmail.com>
2017-06-12 16:09:46 +0100

    fix(ci:publish) add missing index.js file

---
+++ ci/index.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var path = require("path");
+
+module.exports = function (gulpWrapper, ctx) {
+    // Get all files in this folder, except this one
+    var files = fs.readdirSync(__dirname).filter(function(file) {
+        return !file.endsWith("index.js") && fs.statSync(path.join(__dirname, file)).isFile();
+    });
+
+    // Require all files with given gulp and context
+    for(var file of files) {
+        require(path.join(__dirname, file))(gulpWrapper, ctx);
+    }
+};
