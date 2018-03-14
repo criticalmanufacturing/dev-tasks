5eb17d20d93dd0966ef3c331a9c1b7c170baa050
MicaelQueiroz <micael.moreira.queiroz@gmail.com>
2018-03-14 15:50:41 +0000

    feat(npmTasks): added some usefull npm related tasks #25339 (#46)
    * feat(npmTasks): added some usefull npm related tasks #25339
    
    * chore(bumpVersion): #25339
    
    * fix(cmTaks): minor comments  update #25339

--- ci/cm.js
+++ ci/cm.js
@@ -3,6 +3,7 @@ var path = require("path");
 
 var args = require("yargs").argv;
 
+var execSync = require('child_process').execSync;
 var shell = require('gulp-shell');
 var gulpUtil = require("gulp-util");
 
@@ -10,8 +11,13 @@ module.exports = function (gulpWrapper, ctx) {
     var gulp = gulpWrapper.gulp;
     
     // Parse global parameters that need to be used by this plugin
-    // Read parameters
+    // used by tasks check-version, set-version and npm-dist-tag-add
     var version = args.version;
+	// used by task npm-dist-tag-copy-version
+	var sourceTag = args.sourceTag;
+	var targetTag = args.targetTag;
+	// used by tasks npm-dist-tag-add and npm-dist-tag-rm
+	var tag = args.tag;
 
     /**
      * Task: check-version
@@ -59,5 +65,121 @@ module.exports = function (gulpWrapper, ctx) {
             tasks,
             done
         );
+    });
+	
+	/**
+     * Task: npm-dist-tags-versions
+     * Gets an object containing the versions by dist-tags
+     */
+    function getNpmDistTagsVersions(packageName) {
+		// return object {<dist-tag>: <version>}
+		var rt = {};
+		var temp;
+		var distTags = execSync(`npm dist-tag ls ${packageName}`).toString().split("\n");
+		
+		// process result and extract versions
+		for (let i = 0; i < distTags.length; i++) {
+			temp = distTags[i].split(":");
+			if (temp.length === 2) {
+				rt[temp[0].trim()] = temp[1].trim();	
+			}
+		}
+		return rt;
+    };
+	
+	// Register task to add a given npm channel
+    gulp.task("npm-dist-tag-add", function(done) {
+        var tasks = [];
+
+		// Check if there is package.json
+        if (fs.existsSync(path.join(ctx.baseDir, "package.json"))) {
+			// get package config
+			var packageConfig = JSON.parse(fs.readFileSync(path.join(ctx.baseDir, "package.json"), 'utf8'));
+			// Check that both version and tag were provided
+			if (version != null && tag != null) {
+				execSync(`npm dist-tag add ${packageConfig.name}@${version} ${tag}`);
+				gulpUtil.log(
+					gulpUtil.colors.grey("Added dist-tag"), gulpUtil.colors.green(`${tag}: ${version}`));
+			} else {
+				throw new Error("Must provide both --version and --tag");
+			}
+		}
+		done();
+    });
+	
+	// Register task to delete a given npm channel
+    gulp.task("npm-dist-tag-rm", function(done) {
+        var tasks = [];
+
+		// Check if there is package.json
+        if (fs.existsSync(path.join(ctx.baseDir, "package.json"))) {
+			// get package config
+			var packageConfig = JSON.parse(fs.readFileSync(path.join(ctx.baseDir, "package.json"), 'utf8'));
+			// Check that tag was provided
+			if (tag != null) {
+				// get versions {<dist-tag>: <version>}
+				var versions = getNpmDistTagsVersions(packageConfig.name);
+				// Check if source tag exist in npm
+				if (versions[tag] != null) {
+					execSync(`npm dist-tag rm ${packageConfig.name} ${tag}`);
+					gulpUtil.log(gulpUtil.colors.grey("Removed dist-tag"), gulpUtil.colors.green(`${tag}: ${versions[tag]}`));
+				} else {
+					gulpUtil.log(gulpUtil.colors.yellow(`Provided tag (${tag}) doesn't exist in package ${packageConfig.name}`));
+				}
+			} else {
+				throw new Error("Must provide --tag");
+			}
+		}
+		done();
+    });
+	
+	// Register task to output the current npm channels
+    gulp.task("npm-dist-tag-ls", function(done) {
+        var tasks = [];
+
+		// Check if there is package.json
+        if (fs.existsSync(path.join(ctx.baseDir, "package.json"))) {
+			// get package config
+			var packageConfig = JSON.parse(fs.readFileSync(path.join(ctx.baseDir, "package.json"), 'utf8'));
+			// get versions {<dist-tag>: <version>}
+			var versions = getNpmDistTagsVersions(packageConfig.name);
+			// Check if source tag exist in npm
+			if (versions != null) {
+				Object.keys(versions).forEach((key) => {
+					gulpUtil.log(gulpUtil.colors.green(`${key}: ${versions[key]}`));
+				});
+			} else {
+				gulpUtil.log(gulpUtil.colors.yellow(`No versions to display for package ${packageConfig.name}`));
+			}
+		}
+		done();
+    });
+	
+	// Register task to set version of a given npm channel based on another
+    gulp.task("npm-dist-tag-copy-version", function(done) {
+        var tasks = [];
+
+		// Check if there is package.json
+        if (fs.existsSync(path.join(ctx.baseDir, "package.json"))) {
+			// get package config
+			var packageConfig = JSON.parse(fs.readFileSync(path.join(ctx.baseDir, "package.json"), 'utf8'));
+			// Check that both dist tags were provided
+			if (sourceTag != null && targetTag != null) {
+				// get versions {<dist-tag>: <version>}
+				var versions = getNpmDistTagsVersions(packageConfig.name);
+				// Check if source tag exist in npm
+				if (versions[sourceTag] != null) {
+					execSync(`npm dist-tag add ${packageConfig.name}@${versions[sourceTag]} ${targetTag}`);
+					gulpUtil.log(
+						gulpUtil.colors.grey("Copied from tag"), gulpUtil.colors.green(`${sourceTag}: ${versions[sourceTag]}`),
+						gulpUtil.colors.grey("to tag"), gulpUtil.colors.blue(targetTag));
+				} else {
+					gulpUtil.log(gulpUtil.colors.yellow(`Provided source tag (${sourceTag}) doesn't exist in package ${packageConfig.name}`));
+				}
+			} else {
+				throw new Error("Must provide both --sourceTag and --targetTag");
+			}
+		}
+		done();
     });
 }
